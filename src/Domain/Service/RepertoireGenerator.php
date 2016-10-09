ca5265c7fa0fc35246d5da1e1303e6f9b1cc568b
Jonathan Gimeno <jgimeno@gmail.com>
2016-10-09 20:54:58 +0200

    Add new option to the generator to create repertoire with 2 parts.
    - When you create a repertoire with 2 parts, what you really want is
    that the first one belongs to Beatles 1st era but the second part mixes
    era 2 and 3. This is what's happening now.

--- src/Domain/Service/RepertoireGenerator.php
+++ src/Domain/Service/RepertoireGenerator.php
@@ -2,7 +2,9 @@
 
 namespace Repertoire\Domain\Service;
 
+use Doctrine\Common\Collections\ArrayCollection;
 use Repertoire\Domain\Band;
+use Repertoire\Domain\Constant\SongEra;
 use Repertoire\Domain\Exception\BandDoesNotKnowSongsException;
 use Repertoire\Domain\Repertoire;
 use Repertoire\Domain\RepertoirePart;
@@ -12,6 +14,48 @@ class RepertoireGenerator
 {
     public function generateRepertoireFor(Band $band, RepertoireName $repertoireName, $numSongs = 10, $numParts = 1)
     {
+        $this->checkIfBandKnowAnySong($band);
+
+        $repertoire = Repertoire::withName($repertoireName);
+
+        if ($numParts == 1) {
+            $songsBandKnows = $band->getSongsWeKnow();
+            $repertoirePart = $this->createRepertoirePart($numSongs, $songsBandKnows);
+            $repertoire->addPart($repertoirePart);
+        } else {
+            $songsKnownFronEra1 = $band->getSongsWeKnowByEra(SongEra::FIRST_ERA);
+            $songsKnownFronEra2 = $band->getSongsWeKnowByEra(SongEra::SECOND_ERA);
+            $songsKnownFronEra3 = $band->getSongsWeKnowByEra(SongEra::THIRD_ERA);
+
+            $part1 = $this->createRepertoirePart($numSongs / 2, $songsKnownFronEra1);
+            $part2 = $this->createRepertoirePart($numSongs / 2, new ArrayCollection(array_merge($songsKnownFronEra2->toArray(), $songsKnownFronEra3->toArray())));
+            $repertoire->addPart($part1);
+            $repertoire->addPart($part2);
+        }
+
+        return $repertoire;
+    }
+
+    /**
+     * Creates a Playlist Part based on num of songs and the songs a band knows.
+     * @param $numSongs
+     * @param $songsBandKnows
+     * @return RepertoirePart
+     */
+    private function createRepertoirePart($numSongs, ArrayCollection $songsBandKnows):RepertoirePart
+    {
+        $repertoirePart = new RepertoirePart([
+            array_rand($songsBandKnows->toArray(), $numSongs)
+        ]);
+        return $repertoirePart;
+    }
+
+    /**
+     * @param Band $band
+     * @throws BandDoesNotKnowSongsException
+     */
+    private function checkIfBandKnowAnySong(Band $band):void
+    {
         if (!$band->knowsAnySong()) {
             throw new BandDoesNotKnowSongsException(
                 sprintf(
@@ -20,16 +64,5 @@ class RepertoireGenerator
                 )
             );
         }
-
-        $songsBandKnows = $band->getSongsWeKnow();
-
-        $repertoirePart = new RepertoirePart([
-            array_rand($songsBandKnows->toArray(), $numSongs)
-        ]);
-
-        $repertoire = Repertoire::withName($repertoireName);
-        $repertoire->addPart($repertoirePart);
-
-        return $repertoire;
     }
 }
--- tests/Domain/Service/RepertoireGeneratorTest.php
+++ tests/Domain/Service/RepertoireGeneratorTest.php
@@ -67,6 +67,33 @@ class RepertoireGeneratorTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals(1, count($repertoire->getParts()));
     }
 
+    public function testItCreatesARepertoireWith2PartsAndMixesSecondAndThirdEra()
+    {
+        $band = \Mockery::mock(Band::class);
+        $band->shouldReceive('knowsAnySong')
+            ->once()
+            ->andReturn(true);
+
+        $band->shouldReceive('getSongsWeKnow')
+            ->andReturn($this->getBatchOfSongs());
+
+        $band->shouldReceive('getSongsWeKnowByEra')
+            ->with(SongEra::FIRST_ERA)
+            ->andReturn($this->getSongsFrom1Era());
+
+        $band->shouldReceive('getSongsWeKnowByEra')
+            ->with(SongEra::SECOND_ERA)
+            ->andReturn($this->getSongsFrom2Era());
+
+        $band->shouldReceive('getSongsWeKnowByEra')
+            ->with(SongEra::THIRD_ERA)
+            ->andReturn($this->getSongsFrom3Era());
+
+        $repertoire = $this->generator->generateRepertoireFor($band, new RepertoireName("Castañada"), 4, 2);
+
+        $this->assertEquals(2, count($repertoire->getParts()));
+    }
+
     protected function getBatchOfSongs()
     {
         return new ArrayCollection(
@@ -75,6 +102,46 @@ class RepertoireGeneratorTest extends \PHPUnit_Framework_TestCase
                 Song::withNameAndEra("The Long and winding road", SongEra::FIRST_ERA),
                 Song::withNameAndEra("Hey jude", SongEra::FIRST_ERA),
                 Song::withNameAndEra("I want to hold your hand", SongEra::FIRST_ERA),
+                Song::withNameAndEra("Let It Be", SongEra::THIRD_ERA),
+                Song::withNameAndEra("Hey Jude", SongEra::THIRD_ERA),
+                Song::withNameAndEra("Sgt Pepper", SongEra::SECOND_ERA),
+                Song::withNameAndEra("Penny Lane", SongEra::SECOND_ERA),
+            ]
+        );
+    }
+
+    protected function getSongsFrom1Era()
+    {
+        return new ArrayCollection(
+            [
+                Song::withNameAndEra("Let it be", SongEra::FIRST_ERA),
+                Song::withNameAndEra("The Long and winding road", SongEra::FIRST_ERA),
+                Song::withNameAndEra("Hey jude", SongEra::FIRST_ERA),
+                Song::withNameAndEra("I want to hold your hand", SongEra::FIRST_ERA),
+                Song::withNameAndEra("Let It Be", SongEra::THIRD_ERA),
+                Song::withNameAndEra("Hey Jude", SongEra::THIRD_ERA),
+                Song::withNameAndEra("Sgt Pepper", SongEra::SECOND_ERA),
+                Song::withNameAndEra("Penny Lane", SongEra::SECOND_ERA),
+            ]
+        );
+    }
+
+    protected function getSongsFrom2Era()
+    {
+        return new ArrayCollection(
+            [
+                Song::withNameAndEra("Sgt Pepper", SongEra::SECOND_ERA),
+                Song::withNameAndEra("Penny Lane", SongEra::SECOND_ERA),
+            ]
+        );
+    }
+
+    protected function getSongsFrom3Era()
+    {
+        return new ArrayCollection(
+            [
+                Song::withNameAndEra("Let It Be", SongEra::THIRD_ERA),
+                Song::withNameAndEra("Hey Jude", SongEra::THIRD_ERA),
             ]
         );
     }
